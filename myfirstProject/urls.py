6f8e80fbe11f0a5098a0c80a870a432cb425f910
ruhillo <mr.ruhillo@gmail.com>
2020-12-19 21:23:18 +0500

    add reversed function

--- myfirstProject/urls.py
+++ myfirstProject/urls.py
@@ -20,5 +20,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about),
+    path('reversed/', views.reverse,name='reverse'),
     path('', views.home),
 ]
--- myfirstProject/views.py
+++ myfirstProject/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+def reverse(request):
+    userText=str(request.GET['userText'])
+    reversedUserText=userText[::-1]
+    return render(request,'reverse.html',{"reversedUserText":reversedUserText,'userText':userText})
+
 def home(request):
     return render(request,'home.html',{"word":"hello"})
 
